d0e11c1353be9676572614dab1ab2db65d34522a
colthreepv <valerio.coltre@gmail.com>
2021-02-14 21:55:50 +0100

    WIP: first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const got = require('got')
+
+const { readdirSync } = require('fs')
+
+const dir = readdirSync('/mnt/c/Users/valerio/Desktop/quest2/CustomSongs_PC')
+const songsId = dir.map(folderName => {
+  const splitName = folderName.split(' ')
+  return splitName[0]
+})
+
+const shorterSongs = [songsId[0]]
+
+const requests = shorterSongs.forEach(async (songId) => {
+  const lowerCaseSongId = songId.toLowerCase()
+  console.log('url', `https://beatsaver.com/api/download/key/${lowerCaseSongId}`)
+  const request = await got(`https://beatsaver.com/api/download/key/${lowerCaseSongId}`, {
+    headers: { 'User-Agent': 'PostmanRuntime/7.26.10' },
+    followRedirect: false,
+  })
+  console.log(request)
+})
